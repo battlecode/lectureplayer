eb19f4163b95bd5349749096333bf30087d60441
Nate Foss <npfoss@gmail.com>
2020-01-07 19:32:39 -0500

    make miners mine soup

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -69,18 +69,14 @@ public strictfp class RobotPlayer {
 
     static void runMiner() throws GameActionException {
         tryBlockchain();
-        tryMove(randomDirection());
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
-        // tryBuild(randomSpawnedByMiner(), randomDirection());
-        for (Direction dir : directions)
-            tryBuild(RobotType.FULFILLMENT_CENTER, dir);
         for (Direction dir : directions)
             if (tryRefine(dir))
                 System.out.println("I refined soup! " + rc.getTeamSoup());
         for (Direction dir : directions)
             if (tryMine(dir))
                 System.out.println("I mined soup! " + rc.getSoupCarrying());
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
     }
 
     static void runRefinery() throws GameActionException {
